0ba2e8b69a6a722de9f51ca028f534322a5b6dd3
NaturaLog-space <58485181+NaturaLog-space@users.noreply.github.com>
2021-05-16 11:12:43 -0700

    Create Mocha.py

---
+++ Mocha.py
@@ -0,0 +1,27 @@
+'''
+Mocha chocolate is a mix of: 
+Coffee
+Cocoa butter
+Sugar
+Lecithin
+
+Grind coffee
+Sugar
+Cocoa butter
+Lecithin
+'''
+
+print('Grind coffee,sugar,cocoa butter, lecithin for 48 hours')
+print('Once the mocha mix is made')
+print('Time to temper')
+a=float(input('What is the temperature of the mix')
+if a<110:
+    print('Heat Mocha chocolate to 110F')
+else:
+    print('Add cocoa butter or mocha seed crystal')
+    b=True
+    while b:
+        c = float(input('What is the current temperature')
+        if c <= 90:
+            b = False
+    print('Pour Mocha chocolate into mold')
