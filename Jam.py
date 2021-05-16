fbac90bf34e2524fc4020dc315d4ee0180f7acd7
NaturaLog-space <58485181+NaturaLog-space@users.noreply.github.com>
2021-05-16 11:20:50 -0700

    Create Jam.py

---
+++ Jam.py
@@ -0,0 +1,15 @@
+'''
+Raw Jam is a mix of: 
+Dried fruit
+Fresh fruit
+Lemon
+Sugar
+'''
+
+print('Mix Dried fruit, fresh fruit, lemon, sugar')
+def isDissolved:
+    b=bool(input('is the sugar dissolved'))
+    return b
+if isDissolved()==True:
+    print('blend')
+    print('pour')
