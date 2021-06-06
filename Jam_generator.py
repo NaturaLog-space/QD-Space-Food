265e8840f357471c070ecfa166f91ce277de86af
NaturaLog-space <58485181+NaturaLog-space@users.noreply.github.com>
2021-06-06 05:02:52 -0700

    Create first generator
    Batch generator created

---
+++ Jam_generator.py
@@ -0,0 +1,7 @@
+R = (2,1,.5,.00125,.005,.025)
+
+Quantity = int(input('Enter the amount of servings: '))
+UnitSize = int(input('Enter the unit size: '))
+BatchSize = Quantity*UnitSize
+def jam(r,size):
+    return r*size
