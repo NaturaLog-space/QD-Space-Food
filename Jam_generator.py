63aa20774ad8879e7939e88b1d19c227f7963591
NaturaLog-space <58485181+NaturaLog-space@users.noreply.github.com>
2021-06-06 05:17:07 -0700

    Update Jam_generator.py
    Added total batch

--- Jam_generator.py
+++ Jam_generator.py
@@ -5,7 +5,9 @@ UnitSize = int(input('Enter the unit size: '))
 BatchSize = Quantity*UnitSize
 def jam(r,size):
     temp=[]
+    total=0
     for x in r:
         temp.append(x*size)
-    return temp
+        total+=(x*size)
+    return temp,total
 print(jam(R,BatchSize))
